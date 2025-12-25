866866aece453ffc52f9a9f78d993d10cdff9c54
jws <1041139466@qq.com>
2025-12-25 15:20:11 +0800

    接入stripe

--- internal/common/config/viper.go
+++ internal/common/config/viper.go
@@ -1,12 +1,18 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
 
 func NewViperConfig() error {
 	viper.SetConfigName("global")
 	viper.SetConfigType("yaml")
 	// 这个是要在其他服务下面使用，所以路径是这样的
 	viper.AddConfigPath("../common/config")
+	viper.EnvKeyReplacer(strings.NewReplacer("-", "_"))
+	_ = viper.BindEnv("stripe-key", "STRIPE_KEY")
 	viper.AutomaticEnv()
 	return viper.ReadInConfig()
 }
